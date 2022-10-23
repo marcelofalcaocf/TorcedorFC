0db2ef444f1609f4f72f893f6b56e64f64d4355b
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-10-23 15:23:59 -0300

    criei a parte mocada da tabela do brasileirao

---
+++ TorcedorFC/Models/Models(mocados)/Phase.swift
@@ -0,0 +1,19 @@
+//
+//  Phase.swift
+//  TorcedorFC
+//
+//  Created by Marcelo Falcao Costa Filho on 23/10/22.
+//
+
+import Foundation
+
+struct Phase {
+    let gameDay: String
+    let gameTime: String
+    let homeTeam: String
+    let homeTeamLogo: String
+    let scoreboard: String
+    let teamVisitor: Int
+    let teamVisitorLogo: String
+}
+
